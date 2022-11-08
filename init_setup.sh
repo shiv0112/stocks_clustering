0605777226d60cd3051fd5979bb1648583345cd8
Shivansh Srivastava <srivastavashiv0112@gmail.com>
2022-11-08 17:41:37 +0530

    init setup

--- init_setup.sh
+++ init_setup.sh
@@ -4,8 +4,7 @@ conda create --prefix ./env python=3.8 -y
 echo [$(date)]: "Activating the environment"
 source activate ./env
 echo [$(date)]: "Installing the dev env"
-pip install -r requirements_dev.txt
+pip install -r requirements.txt
 echo [$(date)]: "Installing predictions"
-pip install -r prediction_service/requirements_dev.txt
 source activate ./env
 echo [$(date)]: "END"
