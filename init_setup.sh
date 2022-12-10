294502f406d468e20d78238955403a2bc4318966
Shivansh Srivastava <srivastavashiv0112@gmail.com>
2022-12-10 15:40:57 +0530

    init change

--- init_setup.sh
+++ init_setup.sh
@@ -5,6 +5,4 @@ echo [$(date)]: "Activating the environment"
 source activate ./env
 echo [$(date)]: "Installing the dev env"
 pip install -r requirements.txt
-echo [$(date)]: "Installing predictions"
-source activate ./env
 echo [$(date)]: "END"
